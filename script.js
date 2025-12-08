f7449606ab4a14463cc436be87c0e8c4bca999b4
ellineee <nabilah.ramadhani3108@gmail.com>
2025-12-08 17:23:11 +0700

    second commit

--- script.js
+++ script.js
@@ -1,25 +1,7 @@
-document.addEventListener("DOMContentLoaded", () => {
-    const form = document.getElementById("contactForm");
-
-    form.addEventListener("submit", async (e) => {
-        e.preventDefault();
-
-        try {
-            const res = await fetch("https://formspree.io/f/xnnkweaw", {
-                method: "POST",
-                body: new FormData(form),
-                headers: { "Accept": "application/json" }
-            });
-
-            const data = await res.json();
-
-            if (res.ok) {
-                form.reset();
-            } else {
-                alert("Error: " + JSON.stringify(data));
-            }
-        } catch (err) {
-            alert("Gagal mengirim.");
-        }
-    });
-});
+document.getElementById("sub").addEventListener("click", () => {
+        setTimeout(() => {
+          document.getElementById("name").value = "";
+          document.getElementById("email").value = "";
+          document.getElementById("message").value = "";
+        });
+      });
